494a64d71ed4ff3e08975579a8b62e12050d4f2b
Polixa14 <igorpolxx@gmail.com>
2023-06-08 20:02:37 +0200

    rest (api for list/retrieve products)

--- apis/products/views.py
+++ apis/products/views.py
@@ -8,12 +8,3 @@ from rest_framework.response import Response
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductsSerializer
     queryset = Product.objects.all()
-
-    def list(self, request, *args, **kwargs):
-        serializer = ProductsSerializer(self.queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None, *args, **kwargs):
-        product = get_object_or_404(self.queryset, pk=pk)
-        serializer = ProductsSerializer(product)
-        return Response(serializer.data)
